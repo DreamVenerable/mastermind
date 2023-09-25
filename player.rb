a3c928f4bbe7d2da42ef6a2d17712f2d090df868
DreamVenerable <114648602+DreamVenerable@users.noreply.github.com>
2023-09-26 00:14:01 +0200

    removed redundant code

--- player.rb
+++ player.rb
@@ -19,24 +19,10 @@ class Player
       sleep 0.5
       puts "Guesses remaining #{@max_rounds}"
       choice
-      compare_guess(@ai_code.dup, @hint, @input.dup)
+      compare_guess(@input.dup, @hint, @ai_code.dup)
       sleep 0.5
       puts "Your hint: #{@hint.to_s}"
+      p "#{@ai_code}"
     end
-  end
-
-  def compare_guess(code, arr, guess)
-    # Right number & right placement hint
-    code.each_index { |i| correct_place(i, arr, code) if guess[i] - code[i] == 0 }
-
-    # Right number but wrong placement
-    similiar = (code & guess).flat_map { |n| [n]*[guess.count(n), code.count(n)].min }
-    similiar.count.times { |ele| arr.push('W') }
-    return arr
-  end
-
-  def correct_place(i, arr, code)
-    arr.push('R')
-    code[i] = 0
   end
 end
